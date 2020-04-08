b0de49186e246234e703da73deffbcaf42709b45
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-04-07 17:01:01 -0700

    finished product

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Todo = props => {
     return (
         <div 
-        className={`item${props.item.completed ? ' completed' : ''}`}
+        className={`item ${props.item.completed ? ' completed' : ''}`}
         onClick={() => props.toggleItem(props.item.id)}
         >
-        <p>{props.item.todos}</p>
+        <p>{props.item.task}</p>
         </div>
     );
 };
